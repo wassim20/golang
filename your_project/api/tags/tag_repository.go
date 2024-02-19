a62ff49487ec62efe2fce2bfd74e0300c1982b4a
wassim_p <63890903+wassim20@users.noreply.github.com>
2024-02-19 23:36:46 +0000

    half tag

--- your_project/api/tags/tag_repository.go
+++ your_project/api/tags/tag_repository.go
@@ -1,34 +1,25 @@
 package tags
 
 import (
-	"time"
+	"labs/domains"
 
 	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+	"gorm.io/gorm"
 )
 
-// @Description TagPaginator represents the paginated list of tags.
-type TagPaginator struct {
-	Items      []TagTable `json:"items"`      // Items is a slice containing individual tag details.
-	Page       uint       `json:"page"`       // Page is the current page number in the pagination.
-	Limit      uint       `json:"limit"`      // Limit is the maximum number of items per page in the pagination.
-	TotalCount uint       `json:"totalCount"` // TotalCount is the total number of tags in the entire list.
+type Database struct {
+	DB *gorm.DB
 }
 
-// @Description TagIn represents the input structure for creating a new tag.
-type TagIn struct {
-	Name string `json:"name"` // Tag's name.
+// NewTagRepository performs automatic migration of tag-related structures in the database.
+func NewTagRepository(db *gorm.DB) {
+	if err := db.AutoMigrate(&domains.Tag{}); err != nil {
+		logrus.Fatal("An error occurred during automatic migration of the tag structure. Error: ", err)
+	}
 }
 
-// @Description TagDetails represents detailed information about a specific tag.
-type TagDetails struct {
-	ID        uuid.UUID `json:"id"`         // Unique identifier for the tag.
-	Name      string    `json:"name"`       // Tag's  name.
-	CompanyID uuid.UUID `json:"company_id"` // Tag's company
-	CreatedAt time.Time `json:"created_at"` // CreatedAt is the timestamp indicating when the tag was created.
-}
-
-// @Description TagTable represents a single tag entry in a table.
-type TagTable struct {
-	ID   uuid.UUID `json:"id"`   // Unique identifier for the tag.
-	Name string    `json:"name"` // Tag's full name.
+func ReadAllPagination(db *gorm.DB, model []domains.Tag, modelID uuid.UUID, limit, offset int) ([]domains.Tag, error) {
+	err := db.Where("id = ? ", modelID).Limit(limit).Offset(offset).Find(&model).Error
+	return model, err
 }
--- your_project/api/tags/tag_response.go
+++ your_project/api/tags/tag_response.go
@@ -1,1 +1,34 @@
 package tags
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+// @Description TagPaginator represents the paginated list of tags.
+type TagPaginator struct {
+	Items      []TagTable `json:"items"`      // Items is a slice containing individual tag details.
+	Page       uint       `json:"page"`       // Page is the current page number in the pagination.
+	Limit      uint       `json:"limit"`      // Limit is the maximum number of items per page in the pagination.
+	TotalCount uint       `json:"totalCount"` // TotalCount is the total number of tags in the entire list.
+}
+
+// @Description TagIn represents the input structure for creating a new tag.
+type TagIn struct {
+	Name string `json:"name"` // Tag's name.
+}
+
+// @Description TagDetails represents detailed information about a specific tag.
+type TagDetails struct {
+	ID        uuid.UUID `json:"id"`         // Unique identifier for the tag.
+	Name      string    `json:"name"`       // Tag's  name.
+	CompanyID uuid.UUID `json:"company_id"` // Tag's company
+	CreatedAt time.Time `json:"created_at"` // CreatedAt is the timestamp indicating when the tag was created.
+}
+
+// @Description TagTable represents a single tag entry in a table.
+type TagTable struct {
+	ID   uuid.UUID `json:"id"`   // Unique identifier for the tag.
+	Name string    `json:"name"` // Tag's full name.
+}
